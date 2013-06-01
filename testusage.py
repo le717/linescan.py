97877ec2820c3d7102505de9c4dfe4f9e7c0d370
le717 <le717@e.mail>
2013-06-01 15:32:26 -0400

    Improved method of scanning test file
    I see my line number handling on scanlines() is broken...

--- testusage.py
+++ testusage.py
@@ -5,9 +5,38 @@
 linescan.py Test Script for Travis CI
 """
 import linescan
+import sys
+
+text = '''This is a line of text.
+...
+"The Brickster, the most dangerous man on the Island, is in jail now, but for how long?"
+5
+HELLO MY BABY, HELLO MY HONEY! HELLO MY RAG TIME GAL!
+SEND ME A KISS BY WIRE, BABY MY HEART'S ON FIRE!
+*changes channel*
+"New! Hear LEGO minifigures talk for the first time in a LEGO Game!"
+:| HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA
+HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA
+HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA
+*finds different YT video*
+"BOO!"
+"AAAAAAAHHHHHHHHH!!!!!!!!!!"
+"Ha ha, I scared you! You owe me a soda! Heheh"
+'''
 
 if __name__ == "__main__":
-    line = linescan.scanline("NyanMe20.PiP", 14, None)
-    print(line)
-    lines = linescan.scanlines("NyanMe20.PiP", 9, 14, "cp1252")
-    print(lines)
+    command = sys.argv[1]
+
+    # The command to write the test file
+    if command == "write":
+        with open("Thisisafile.txt", "wt") as f:
+            f.write(text)
+
+    # The command to scan the test file
+    elif command == "scanme":
+        line = linescan.scanline("Thisisafile.txt", 3, None)
+        print(line)
+        lines = linescan.scanlines("Thisisafile.txt", 5, 8, "cp1252")
+        print(lines)
+        thisshouldbefalse = linescan.scanline("Thisisafile.txt", 16, None)
+        print(thisshouldbefalse)
