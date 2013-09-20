fb7d4d67419ed1356e3a93a9c592da897dafcd95
Triangle717 <le717@e.mail>
2013-09-20 11:05:38 -0400

    [ci skip] Updated variables, added example usage under
    `if __name__ == "__main__":` cover.

--- linescan.py
+++ linescan.py
@@ -19,6 +19,7 @@
     along with linescan.py. If not, see <http://www.gnu.org/licenses/>.
 """
 
+from __future__ import print_function
 import sys
 import locale
 
@@ -26,10 +27,10 @@ import locale
 if sys.version_info < (3, 0):
     from io import open
 
-#TODO: Store and retrieve scans so they are not lost upon a new scam
+#TODO: Store and retrieve scans so they are not lost upon a new scan.
 
 
-def scanline(file, lineno, encode=None):
+def scanline(myfile, lineno, encode=None):
     """Reads a single line from a file using a specified encoding.
     Falls back to default system encoding if None is specified."""
     try:
@@ -43,21 +44,19 @@ def scanline(file, lineno, encode=None):
             encode = locale.getpreferredencoding(False)
 
         # Using the recommended with handle, read the specified line number,
-        with open(file, "rt", encoding=encode) as f:
+        with open(myfile, "rt", encoding=encode) as f:
             line = f.readlines()[lineno]
 
         # and remove the trailing new line.
         line = line.strip()
-
-        # Send back the line
         return line
 
+    # Return False if there is any error.
     except Exception:
-        # Return False if there is any error.
         return False
 
 
-def scanlines(file, startlineno, endlineno, encode=None):
+def scanlines(myfile, startlineno, endlineno, encode=None):
     """Reads multiple lines from a file."""
     try:
         # Since line numbers start at 0,
@@ -70,20 +69,37 @@ def scanlines(file, startlineno, endlineno, encode=None):
             encode = locale.getpreferredencoding(False)
 
         # Using the recommended with handle, read the specified lines.
-        with open(file, "rt", encoding=encode) as f:
+        with open(myfile, "rt", encoding=encode) as f:
             lines = f.readlines()[startlineno:endlineno]
 
         # Next, we break the lines from the list,
         lines = "".join(lines)
         # and remove the trailing new line.
         lines = lines.strip()
-
-        # Send back the lines
         return lines
 
+    # Return False if there is any error.
     except Exception:
-       # Return False if there is any error.
         return False
 
 if __name__ == "__main__":
+    print('''
+linescan.py Created 2013 Triangle717
+<http://Triangle717.WordPress.com/>
+Released under the GNU General Public License version 3
+<http://www.gnu.org/licenses/>.
+
+Example linescan.py usage:
+
+from __future__ import print_function
+import linescan
+
+# Read a single line using UTF-8 encoding
+myline = linescan.scanline("MyFile.txt", 5, "utf-8")
+print(mylines)
+
+# Read multiple lines using system default encoding
+mytwolines = linescan.scanline("MyFile.txt", 2, 5)
+print(mytwolines)
+''')
     sys.exit(0)
