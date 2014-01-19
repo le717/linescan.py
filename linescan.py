82c25e73cde368d20543c12ac82a8a8cfcd3a9ff
Triangle717 <le717@users.noreply.github.com>
2014-01-19 16:37:45 -0500

    Slowly refactoring script with new store system and D.R.Y.
    I doubt this will build, which is fine. I'm still working on it. :smiley:

--- linescan.py
+++ linescan.py
@@ -17,22 +17,28 @@ import locale
 if sys.version_info < (3, 0):
     from io import open
 
-#TODO: Store and retrieve scans so they are not lost upon a new scan.
-
 # Store the user's scans for later retrieval
 myScans = {}
 
 
 def clearscans():
-    """Clear stored scans"""
+    """Clear any stored scans"""
     global myScans
     myScans = {}
 
 
-def scan(file, startLine, endLine=None):
+def scan(myFile, startLine, endLine=None, encode=None):
+    """
+    myFile: the file to read.
+    startLine: The line you wish to read.
+    endLine (optional): The last line to want to read.
+    Specify when reading multiple lines.
+    encode (optional): Specify an file encoding to use.
+    Defaults to default system encoding.
+    """
 
     # Construct the comma-separated pointer for this file,
-    filePointer = "{0},{1}".format(file, startLine)
+    filePointer = "{0},{1}".format(myFile, startLine)
 
     # Append the ending line if the user specifies one
     if endLine is not None:
@@ -41,40 +47,49 @@ def scan(file, startLine, endLine=None):
 
     # If the pointer has been been used, return the stored value
     if filePointer in myScans:
-        print(myScans[filePointer])
+        return myScans[filePointer]
 
-    # The pointer could not be found
+    # The pointer could not be found, proceed to read the file
     else:
         #print("DEBUG: Pointer not found")
+
+        # Use the system default encoding, if one is not specified.
+        if encode is None:
+            encode = locale.getpreferredencoding(False)
+
         if endLine is None:
-            theScan = _scanline(file, startLine)
+            theScan = _scanline(myFile, startLine, endLine, encode)
         else:
-            #theScan = _scanlines(file, startLine, endLine)
+            #theScan = _scanlines(myFile, startLine, endLine)
             print("DEBUG: _scanlines()")
 
         myScans[filePointer] = theScan
+        return theScan
 
 
-def scanline(myfile, lineno, encode=None):
+def _scanline(myFile, startLine, endLine, encode):
     """Reads a single line from a file using a specified encoding.
     Falls back to default system encoding if None is specified."""
     try:
         # Since line numbers start at 0,
         # get the starting line number.
-        lineno = lineno - 1
+        startLine -= 1
 
-        if encode is None:
-            # If no encoding is specified, use the default encoding;
-            # Otherwise, use speficied encoding
-            encode = locale.getpreferredencoding(False)
+        with open(myFile, "rt", encoding=encode) as f:
+            if endLine is None:
+                # Read only one line
+                lines = f.readlines()[startLine]
+            # Read multiple lines
+            else:
+                lines = f.readlines()[startLine:endLine]
 
-        # Using the recommended with handle, read the specified line number,
-        with open(myfile, "rt", encoding=encode) as f:
-            line = f.readlines()[lineno]
+        if endLine is not None:
+            # Break the multiple lines from the list
+            lines = "".join(lines)
 
-        # and remove the trailing new line.
-        line = line.strip()
-        return line
+        # Remove any trailing new lines.
+        lines = lines.strip()
+        return lines
 
     # Return False if there is any error.
     except Exception:
@@ -86,11 +101,11 @@ def scanlines(myfile, startlineno, endlineno, encode=None):
     try:
         # Since line numbers start at 0,
         # get the starting line number.
-        startlineno = startlineno - 1
+        startlineno -= 1
 
         if encode is None:
             # If no encoding is specified, use the default encoding;
-            # Otherwise, use speficied encoding
+            # Otherwise, use specified encoding
             encode = locale.getpreferredencoding(False)
 
         # Using the recommended with handle, read the specified lines.
@@ -116,10 +131,12 @@ if __name__ != "__main__":
 
 if __name__ == "__main__":
     print('''
-linescan.py Created 2013 Triangle717
+linescan.py - Effortlessly read lines from a text file using any encoding
+Created 2013-2014 Triangle717
 <http://Triangle717.WordPress.com/>
-Released under the GNU General Public License version 3
-<http://www.gnu.org/licenses/>.
+
+Licensed under The MIT License
+<http://opensource.org/licenses/MIT/>
 
 Example linescan.py usage:
 
