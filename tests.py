4c49cb93e51ea71ee812b3272352657fbf3daf05
Triangle717 <le717@hotmail.com>
2014-01-20 14:58:54 -0500

    Update tests.py with most of new API,
    Having trouble with the last ones. I want to split these into separate
    scripts in the test/ directory, but I can't seem to import linescan from
    the root.

--- tests.py
+++ tests.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 """
-    linescan.py - Effortlessly read lines from a text file using any encoding
+    linescan.py
+    Effortlessly read a text file using counting numbers.
+
     Created 2013-2014 Triangle717
     <http://Triangle717.WordPress.com/>
 
@@ -18,17 +20,34 @@ testFile = os.path.join("test", "testfile.txt")
 
 if __name__ == "__main__":
     # Scan a single line
-    line = linescan.scan(testFile, 5, encode="utf_8")
+    line = linescan.scan(testFile, 5, encoding="utf_8")
     print(line)
 
-    # Scan the same line as above, returns the previous reading
-    lineRepeat = linescan.scan(testFile, 5, encode="utf_8")
+    # Scan the same line as above, returns the previous reading.
+    lineRepeat = linescan.scan(testFile, 5, encoding="utf_8")
     print(lineRepeat)
 
-    # Scan multiple lines
-    lines = linescan.scan(testFile, 8, 12, encode="cp1252")
+    # Scan multiple lines.
+    lines = linescan.scan(testFile, 8, 12, encoding="cp1252")
     print(lines)
 
-    # Perform an invalid reading
-    thisIsFalse = linescan.scan(testFile, 55)
+    # Attempt to read a nonexistant file.
+    thisIsFalse = linescan.scan("fake.txt", 55)
     print(thisIsFalse)
+
+    # Any error can be caught by checking it's boolean value.
+    if not thisIsFalse:
+        print('An error occured while scanning "fake.txt"')
+
+    # Raise an exception upon error.
+    #linescan.showerrors(True)
+
+    # Attempt to read the nonexistant file again,
+    # but this time with exceptions enabled.
+    #thisIsAnException = linescan.scan("fake.txt", 55)
+
+    # Exceptions can be caught using try.. except
+    #try:
+        #thisIsFalseThree = linescan.scan("fake.txt", 55)
+    #except FileNotFoundError:
+        #print("ERROR")
