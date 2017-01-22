6b921049b9404a995ad64c46788aa69cc122a10f
Caleb Ely <le717.code@yahoo.com>
2017-01-22 15:01:24 -0500

    Fix running tests from root level

--- tests/test_linescan.py
+++ tests/test_linescan.py
@@ -3,7 +3,7 @@ import os
 import sys
 import unittest
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("."))
 
 import testhelpers
 from src import linescan
--- tests/testhelpers.py
+++ tests/testhelpers.py
@@ -2,7 +2,7 @@
 import os
 
 
-TEST_FILES_ROOT_PATH = os.path.join("..", "examples")
+TEST_FILES_ROOT_PATH = os.path.join(os.getcwd(), "examples")
 TEST_FILES_TESTFILE = os.path.join(
     TEST_FILES_ROOT_PATH,
     "testfile.txt"
