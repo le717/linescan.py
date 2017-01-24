256c0abf53e599647088f659ddde36b25ec89687
Caleb Ely <le717.code@yahoo.com>
2017-01-24 16:24:32 -0500

    Test location according to execution order

--- tests/test_linescan.py
+++ tests/test_linescan.py
@@ -18,21 +18,6 @@ class TestLineScan(unittest.TestCase):
         global ls
         ls = linescan.LineScan()
 
-    def test_scan_single_line_utf8(self):
-        """Test single line scanning using UTF-8."""
-        line = ls.scan(testhelpers.TEST_FILES_TESTFILE, 6, encoding="utf-8")
-        self.assertEqual(line, "revolutionary ROI.\n")
-
-    def test_scan_multiple_lines_cp1252(self):
-        """Test multiple line scanning using cp1252."""
-        lines = ls.scan(testhelpers.TEST_FILES_TESTFILE,
-                        8, 12, encoding="cp1252")
-        self.assertEqual(lines, """Taken from http://tvipsum.com/
-The weather started getting rough - the tiny ship was tossed. If not for the
-courage of the fearless crew the Minnow would be lost. the Minnow would
-be lost. So get a witch's shawl on a broomstick you can crawl on. Were
-gonna pay a call on the Addams Family. The Love Boat soon\n""")
-
     def test_clear_all(self):
         """
         Test running `clear()` with no parameter
@@ -44,7 +29,7 @@ gonna pay a call on the Addams Family. The Love Boat soon\n""")
     def test_clear_non_existent_file(self):
         """
         Test passing a non-existent file name to `clear()`
-        and the cache does not change sizes
+        and the cache does not change sizes.
         """
         ls.clear("404-file-not-found.txt")
         self.assertEqual(len(ls), 0)
@@ -65,12 +50,6 @@ gonna pay a call on the Addams Family. The Love Boat soon\n""")
         ls.scan(testhelpers.TEST_FILES_TESTFILE, 6, encoding="utf-8")
         self.assertEqual(len(ls), 2)
 
-    def test_return_false_on_error(self):
-        """Test returning False when an error occurs."""
-        ls.show_errors(False)
-        result = ls.scan("404-file-not-found.txt", 1)
-        self.assertFalse(result)
-
     def test_raise_exception_on_error(self):
         """
         Test raising a FileNotFoundError exception
@@ -79,6 +58,27 @@ gonna pay a call on the Addams Family. The Love Boat soon\n""")
         ls.show_errors(True)
         with self.assertRaises(FileNotFoundError):
             ls.scan("404-file-not-found.txt", 1)
+
+    def test_return_false_on_error(self):
+        """Test returning False when an error occurs."""
+        ls.show_errors(False)
+        result = ls.scan("404-file-not-found.txt", 1)
+        self.assertFalse(result)
+
+    def test_scan_multiple_lines_cp1252(self):
+        """Test multiple line scanning using cp1252."""
+        lines = ls.scan(testhelpers.TEST_FILES_TESTFILE,
+                        8, 12, encoding="cp1252")
+        self.assertEqual(lines, """Taken from http://tvipsum.com/
+The weather started getting rough - the tiny ship was tossed. If not for the
+courage of the fearless crew the Minnow would be lost. the Minnow would
+be lost. So get a witch's shawl on a broomstick you can crawl on. Were
+gonna pay a call on the Addams Family. The Love Boat soon\n""")
+
+    def test_scan_single_line_utf8(self):
+        """Test single line scanning using UTF-8."""
+        line = ls.scan(testhelpers.TEST_FILES_TESTFILE, 6, encoding="utf-8")
+        self.assertEqual(line, "revolutionary ROI.\n")
 
 
 if __name__ == "__main__":
